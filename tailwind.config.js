946546852377101afb16776a0ca3a770478d4ca8
Nick Messing <dot.nick.dot.messing@gmail.com>
2021-07-09 15:58:48 +0300

    Proper tailwind purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  purge: { content: ['./public/**/*.html', './src/**/*.tsx'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
